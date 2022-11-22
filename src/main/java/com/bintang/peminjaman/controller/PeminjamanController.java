4f200449f431108d5f18506ccb032490bc23bd8a
bintang1804 <80028370+bintang1804@users.noreply.github.com>
2022-11-22 13:23:39 +0700

    Initial commit

---
+++ src/main/java/com/bintang/peminjaman/controller/PeminjamanController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bintang.peminjaman.controller;
+
+import com.bintang.peminjaman.VO.ResponseTemplateVO;
+import com.bintang.peminjaman.entity.Peminjaman;
+import com.bintang.peminjaman.service.PeminjamanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author LABSIDOSEN
+ */
+@RestController
+@RequestMapping("/peminjaman")
+public class PeminjamanController {
+    @Autowired
+    private PeminjamanService peminjamanService;
+    
+    @PostMapping("/")
+    public Peminjaman savePeminjaman(@RequestBody Peminjaman peminjaman){
+        return peminjamanService.savePeminjaman(peminjaman);
+    }
+    
+    @GetMapping("{id}")
+    public ResponseTemplateVO getPeminjaman(@PathVariable("id") Long peminjamanId){
+        return peminjamanService.getPeminjaman(peminjamanId);
+    }
+}
---
+++ src/main/java/com/bintang/peminjaman/service/PeminjamanService.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bintang.peminjaman.service;
+
+import com.bintang.peminjaman.VO.Anggota;
+import com.bintang.peminjaman.VO.Buku;
+import com.bintang.peminjaman.VO.ResponseTemplateVO;
+import com.bintang.peminjaman.entity.Peminjaman;
+import com.bintang.peminjaman.repository.PeminjamanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author LABSIDOSEN
+ */
+@Service
+public class PeminjamanService {
+    @Autowired
+    private PeminjamanRepository peminjamanRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    public Peminjaman savePeminjaman(Peminjaman peminjaman){
+        return peminjamanRepository.save(peminjaman);
+    }
+    
+    public ResponseTemplateVO getPeminjaman(Long peminjamanId){
+        ResponseTemplateVO vo = new ResponseTemplateVO();
+        Peminjaman peminjaman = 
+                peminjamanRepository.findByPeminjamanId(peminjamanId);
+        Anggota anggota = 
+        restTemplate.getForObject("http://localhost:9027/anggota/"
+                + peminjaman.getAnggotaId(), Anggota.class);
+        Buku buku = restTemplate.getForObject("http://localhost:9003/buku/" 
+                + peminjaman.getBukuId(), Buku.class);
+        vo.setPeminjaman(peminjaman);
+        vo.setAnggota(anggota); 
+        vo.setBuku(buku); 
+        return vo;
+    }
+    
+}
